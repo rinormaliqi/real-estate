765116e30a865f093d1a2e39bdd08c20e8f93fd9
rinormaliqi <rmaliqi75@gmail.com>
2026-02-04 14:46:48 +0100

    sas2.3

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,23 +1,16 @@
-// import DashboardProfile from "@/components/dashboard/profile";
-// import Wrapper from "@/layouts/Wrapper";
+import Wrapper from "@/layouts/Wrapper";
 
-// export const metadata = {
-//    title: "Dashboard Profile HOZN - Real Estate React Next js",
-// };
-// const index = () => {
-//    return (
-//       <Wrapper>
-//          <DashboardProfile />
-//       </Wrapper>
-//    )
-// }
-
-// export default index
-
-import React from 'react'
-
-export const page = () => {
-  return (
-    <div>page</div>
-  )
+export const metadata = {
+   title: "Dashboard Profile HOZN - Real Estate React Next js",
+};
+const index = () => {
+   return (
+      <Wrapper>
+         <div>
+            profile
+         </div>
+      </Wrapper>
+   )
 }
+
+export default index;
