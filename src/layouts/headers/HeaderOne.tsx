0bf363418a2dfeb0bf96e275302fa936cae26bf3
rinormaliqi <rmaliqi75@gmail.com>
2026-02-04 14:54:28 +0100

    sas2.5.2

--- src/layouts/headers/HeaderOne.tsx
+++ src/layouts/headers/HeaderOne.tsx
@@ -27,9 +27,9 @@ const HeaderOne = ({ style }: any) => {
                            <li>
                               <Link href="#" data-bs-toggle="modal" data-bs-target="#loginModal" className="btn-one"><i className="fa-regular fa-lock"></i> <span>Login</span></Link>
                            </li>
-                           <li className="d-none d-md-inline-block ms-3">
+                           {/* <li className="d-none d-md-inline-block ms-3">
                               <Link href="/dashboard/add-property" className="btn-two" target="_blank"><span>Add Listing</span> <i className="fa-thin fa-arrow-up-right"></i></Link>
-                           </li>
+                           </li> */}
                         </ul>
                      </div>
                      <nav className="navbar navbar-expand-lg p0 order-lg-2">
